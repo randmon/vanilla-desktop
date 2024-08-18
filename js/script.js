aa1d03971d2c2cba153040df3454e02202d0d604
randmon <ccmarques98@gmail.com>
2024-08-18 18:05:08 +0200

    click anywhere on window to select

--- js/script.js
+++ js/script.js
@@ -2,18 +2,65 @@ const nWindows = 3;
 const debug = true;
 
 let currentZ = 10;
+let currentActiveWindow = "window1";
 
 const log = (message) => {
   if (debug) console.log(message);
 };
 
-const windows = [];
-for (let i = 1; i <= nWindows; i++) {
-  windows.push(document.getElementById(`window${i}`));
+class Application {
+  constructor(id, window, taskbarButton, minimizeButton) {
+    this.id = id;
+    this.window = window;
+    this.taskbarButton = taskbarButton;
+    this.minimizeButton = minimizeButton;
+  }
+
+  setActive(active) {
+    if (active) {
+      this.taskbarButton.classList.add("taskbar-active");
+      this.window.classList.add("window-active");
+      currentActiveWindow = this.id;
+    } else {
+      this.taskbarButton.classList.remove("taskbar-active");
+      this.window.classList.remove("window-active");
+    }
+  }
 }
 
-// Make the windows draggable:
-windows.forEach((w) => makeDraggable(w));
+const applications = [];
+for (let i = 1; i <= nWindows; i++) {
+  const id = `window${i}`;
+  const app = new Application(
+    id,
+    document.getElementById(id),
+    document.getElementById(`${id}-taskbar-button`),
+    document.getElementById(`${id}-minimize-button`)
+  );
+  applications.push(app);
+
+  app.taskbarButton.addEventListener("click", () => {
+    taskbarButtonClicked(app);
+  });
+  app.minimizeButton.addEventListener("click", () => {
+    toggleWindowDisplay(app);
+  });
+  app.window.addEventListener("mousedown", () => {
+    setActiveApp(app);
+  });
+  makeDraggable(app.window);
+}
+setActiveApp(applications[0]);
+
+function setActiveApp(app) {
+  applications.forEach((a) => {
+    a.setActive(false);
+  });
+
+  app.setActive(true);
+  app.window.style.zIndex = currentZ++;
+}
+
 function makeDraggable(elmnt) {
   let pos1 = 0;
   let pos2 = 0;
@@ -35,9 +82,6 @@ function makeDraggable(elmnt) {
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
-
-    elmnt.style.zIndex = currentZ++;
-    log(currentZ);
 
     document.onmousemove = startDragElement;
     document.onmouseup = stopDragElement;
@@ -80,33 +124,46 @@ function makeDraggable(elmnt) {
   }
 }
 
-// Make the windows minimizable:
-windows.forEach((w) => {
-  const windowToggle = document.getElementById(`${w.id}-taskbar-button`);
-  windowToggle.addEventListener("click", () => toggleWindowDisplay(w));
-  const minimizeButton = document.getElementById(`${w.id}-minimize-button`);
-  minimizeButton.addEventListener("click", () => toggleWindowDisplay(w));
-});
+function taskbarButtonClicked(app) {
+  // If app is not visible, show it and set active
+  if (app.window.style.visibility != "visible") {
+    toggleWindowDisplay(app);
+    setActiveApp(app);
+  } else {
+    // if it is the active window, minimize it
+    if (currentActiveWindow == app.id) {
+      toggleWindowDisplay(app);
+    } else {
+      // if it is not the active window, set it as active
+      setActiveApp(app);
+    }
+  }
+}
 
-function toggleWindowDisplay(elmnt) {
-  if (elmnt.style.visibility != "visible") {
-    log(`showing element [${elmnt.id}]`);
+function toggleWindowDisplay(app) {
+  if (app.window.style.visibility != "visible") {
+    log(`showing element [${app.window.id}]`);
 
     // put item in center of screen, 2/3 of screen height, if current position is not set
-    if (elmnt.style.top == "") {
-      elmnt.style.top = window.innerHeight / 3 + "px";
-      elmnt.style.left = window.innerWidth / 2 - elmnt.offsetWidth / 2 + "px";
+    if (app.window.style.top == "") {
+      app.window.style.top = window.innerHeight / 3 + "px";
+      app.window.style.left =
+        window.innerWidth / 2 - app.window.offsetWidth / 2 + "px";
     }
 
-    elmnt.style.visibility = "visible";
-    elmnt.style.zIndex = currentZ++; // Overlay this item on top of everything else
-    elmnt.style.opacity = 1;
+    app.window.style.visibility = "visible";
+    app.window.style.zIndex = currentZ++; // Overlay this item on top of everything else
+    app.window.style.opacity = 1;
+
+    setActiveApp(app);
   } else {
-    log(`hiding element [${elmnt.id}]`);
-    elmnt.style.opacity = "0";
+    log(`hiding element [${app.id}]`);
+    app.window.style.opacity = "0";
+    app.taskbarButton.classList.remove("taskbar-active");
+
     // wait for animation to finish before removing z-index
     setTimeout(() => {
-      elmnt.style.visibility = "hidden";
+      app.window.style.visibility = "hidden";
     }, 200);
   }
 }
@@ -114,29 +171,30 @@ function toggleWindowDisplay(elmnt) {
 // Show desktop button hides all windows
 const desktopButton = document.getElementById("desktop-button");
 desktopButton.addEventListener("click", () => {
-  windows.forEach((window) => {
-    window.style.opacity = "0";
+  applications.forEach((app) => {
+    app.window.style.opacity = "0";
+    app.taskbarButton.classList.remove("taskbar-active");
+
     setTimeout(() => {
-      window.style.visibility = "hidden";
+      app.window.style.visibility = "hidden";
     }, 200);
   });
 });
 
 // on browser window resize, make sure windows are not out of bounds
 window.addEventListener("resize", () => {
-  windows.forEach((w) => checkWindowBounds(w));
+  applications.forEach((app) => checkWindowBounds(app.window));
 });
 
-function checkWindowBounds(elmnt) {
+function checkWindowBounds(windowElement) {
   // if window is out of bounds, move it back to center of screen
-  const maxY = window.innerHeight - elmnt.offsetHeight;
-  const maxX = window.innerWidth - elmnt.offsetWidth;
-  if (elmnt.offsetTop > maxY) {
-    elmnt.style.top = window.innerHeight / 3 + "px";
+  const maxY = window.innerHeight - windowElement.offsetHeight;
+  const maxX = window.innerWidth - windowElement.offsetWidth;
+  if (windowElement.offsetTop > maxY) {
+    windowElement.style.top = window.innerHeight / 3 + "px";
   }
-  if (elmnt.offsetLeft > maxX) {
-    elmnt.style.left = window.innerWidth / 2 - elmnt.offsetWidth / 2 + "px";
+  if (windowElement.offsetLeft > maxX) {
+    windowElement.style.left =
+      window.innerWidth / 2 - windowElement.offsetWidth / 2 + "px";
   }
 }
-
-const homeButton = document.getElementById("home-button");
